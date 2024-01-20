64bd3df5e5203b442156f3ef572e4461d49f75f8
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2024-01-20 21:13:22 +0530

    Adding map functionality to show items

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
+  const [items, setItems] = useState([]);
+
+  const fetchProducts = async () => {
+    const data = await fetch("https://fakestoreapi.com/products");
+    const json = await data.json();
+
+    console.log(json);
+    setItems(json);
+  };
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const data = await fetch("https://fakestoreapi.com/products");
-      const json = await data.json();
-
-      console.log(json);
-      setProducts(json);
-    };
-
     fetchProducts();
   }, []);
+
   return (
     <div>
-      {products.map((product) => {
-        <div className="productCard" key={product.id}>
-          <img src={product.image} />
-          <h4>{product.title}</h4>
-          <h5>{product.price}</h5>
+      {items.map((item) => {
+        <div className="productCard" key={item.id}>
+          <img src={item.image} />
+          <h4>{item.title}</h4>
+          <h5>{item.price}</h5>
           <button className="cartBtn">Add to Cart</button>
         </div>;
       })}
